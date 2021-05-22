b848ba466dc447041b0e5c35bd6e15e0c38cfff9
LautaroRiofrio <70457668+LautaroRiofrio@users.noreply.github.com>
2021-05-22 01:24:01 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const router = express.Router();
+
+app.set("port",3000);
+app.set("views", path.join(__dirname,"views"));
+app.use(express.static(path.join(__dirname,"public")));
+
+
+app.get("/",function(req,res){
+	res.sendFile(path.resolve(__dirname,"views/index.html"));
+})
+
+
+app.listen(app.get("port"),function(){
+	console.log("server on port",app.get("port"));
+})
+
+
+function hola(){
+	console.log(hola)
+}
+
---
+++ src/public/js/main.js
@@ -0,0 +1,169 @@
+
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+var rect = canvas.getBoundingClientRect();
+
+canvas.width = 600;
+canvas.height = 600;
+
+ctx.fillStyle = "#fff";
+ctx.fillRect(0,0,600,600);
+
+var x = 0;
+var y = 0;
+var color = "#000"
+var dibujando = false;
+
+
+canvas.addEventListener("mouseout",function(e){
+	dibujando = false;
+})
+canvas.addEventListener("mousedown",function(e){
+	x = e.clientX - rect.left;
+	y = e.clientY - rect.top;
+	dibujando = true;
+});
+
+
+canvas.addEventListener("mousemove",function(e){
+	if (dibujando == true) {
+		dibujar(x,y,e.clientX - rect.left , e.clientY - rect.top)
+		x = e.clientX - rect.left;
+		y = e.clientY - rect.top;
+
+	}
+})
+
+
+canvas.addEventListener("mouseup",function(e){
+	if (dibujando == true) {
+		dibujar(x,y,e.clientX - rect.left , e.clientY - rect.top)
+		x = 0;
+		y = 0;
+		dibujando = false;
+	}
+})
+
+
+
+function dibujar(x1,y1,x2,y2){
+	ctx.beginPath();
+	ctx.strokeStyle = color;
+	ctx.lineWidth = 2;
+	ctx.moveTo(x1,y1);
+	ctx.lineTo(x2,y2);
+	ctx.stroke();
+	ctx.closePath();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function generarColores(){
+	var div = document.querySelector(".colores");
+	for(let i = 0; i < 20; i++){
+		var r = PasarDecimalAHexa(Math.round(Math.random()*255));
+		var g = PasarDecimalAHexa(Math.round(Math.random()*255));
+		var b = PasarDecimalAHexa(Math.round(Math.random()*255));
+		var hexa = `#${r}${g}${b}`;
+		console.log(hexa)
+		div.innerHTML += `<div class="color" style="background:${hexa}" onclick="cambiarColor('${hexa}')"></div>`
+	}
+}
+
+function cambiarColor(hexa){
+	color =hexa;
+}
+
+
+
+
+
+function PasarDecimalAHexa(num){
+	var decimal = num;
+	var hexa = 0;
+
+	for(let i = 0; i < 16; i++){
+		if (  (i*16**1) > decimal) {
+			hexa = pasarNumeroALetra(i-1);
+			decimal = decimal - ((i-1)*16**1);
+			break;			
+		}
+		if (i == 15) {
+			hexa = pasarNumeroALetra(15);
+			decimal = decimal - (15*16**1);
+		}
+
+	}
+	for(let i = 0; i < 16; i++){
+		if (  (i) > decimal) {
+			if (i != 15) {				
+				hexa += pasarNumeroALetra(i-1);
+				break;
+			}
+		}	
+		if ( i == 15) {
+			if (i >= decimal) {
+				hexa += pasarNumeroALetra(i-1);
+			} else {
+				hexa += pasarNumeroALetra(15);
+			}
+		}
+	}
+	return hexa;
+}
+
+
+
+
+
+
+function pasarNumeroALetra(num){
+	var letras = ["A","B","C","D","E","F"];
+	if (num >= 10) {
+		num = num - 10;
+		return letras[num];
+	} else {
+		return num;
+	}
+}
+
+
+
+
+function download(){
+
+	var c = canvas;
+
+	let filename = prompt("Guardar como",""),
+	link = document.createElement('a');
+
+ 	if (filename.length == 0) {
+
+		link.download = "Sin título";
+		link.href = c.toDataURL("image/png");
+
+	} else{
+
+		link.download = filename;
+		link.href = c.toDataURL("image/png");
+
+	}
+
+	link.click();
+
+}
+
+
